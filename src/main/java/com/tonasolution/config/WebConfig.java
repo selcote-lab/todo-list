58940d2c74315eb31719637995d9fd3fd56d8ebf
zouhaire <zouhaire@idside.de>
2019-11-22 12:22:55 +0100

    added dispatcher servlet

---
+++ src/main/java/com/tonasolution/config/WebConfig.java
@@ -0,0 +1,11 @@
+package com.tonasolution.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "com.tonasolution")
+public class WebConfig {
+}
