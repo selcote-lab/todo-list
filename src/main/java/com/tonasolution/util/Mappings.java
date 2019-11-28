18270600e63d03b8b3f61ab097eac6bed4b86ad2
zouhaire <zouhaire@idside.de>
2019-11-28 12:14:07 +0100

    impl add & edit item feature

--- src/main/java/com/tonasolution/util/Mappings.java
+++ src/main/java/com/tonasolution/util/Mappings.java
@@ -3,7 +3,7 @@ package com.tonasolution.util;
 public final class Mappings {
     // == constants ==
     public static final String ITEMS = "items";
-    public static final String ADD_ITEM = "add_item";
+    public static final String ADD_ITEM = "addItem";
 
     // == constructors ==
     private Mappings() {}
